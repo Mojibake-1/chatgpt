87c865c6f878211f8bec7c948a2316ee4bae0225
Ackyess <133567825+Ackyess@users.noreply.github.com>
2024-05-09 19:18:27 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,25 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <h1 className={styles.title}>ChatGPT可用账号</h1>
 
-{/*       <div className={styles.grid}>
-        <a href="https://via.learnwithgpt.beauty/" className={styles.card}>
-          <h2>Plus &rarr;</h2>
-          <p>
-            Access to GPT-4
-            <br />
-            rate limit :40 messages /3 hours
-          </p>
-        </a>
 
-        <a href="https://via.learnwithgpt.beauty/" className={styles.card}>
-          <h2>Plus &rarr;</h2>
-          <p>
-            {" "}
-            Access to GPT-4
-            <br />
-            rate limit :40 messages /3 hours
-          </p>
-        </a> */}
 
         <a href="https://neu.learnwithgpt.beauty/" className={styles.card}>
           <h2>Team1 &rarr;</h2>
