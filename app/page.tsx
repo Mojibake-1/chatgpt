f4e574f9bcd52bc9e8459144d5b75a0304043e26
Ackyess <133567825+Ackyess@users.noreply.github.com>
2024-06-26 22:12:29 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -99,7 +99,20 @@ export default function Home() {
         url: "https://neu17.learnwithgpt.space/",
         description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
       },
-      
+              title: "午夜时刻",
+        url: "https://neu18.learnwithgpt.space/",
+        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+      },
+      {
+        title: "信仰、希望、爱",
+        url: "https://neu19.learnwithgpt.space/",
+        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+      },
+      {
+        title: "至上主义",
+        url: "https://neu20.learnwithgpt.space/",
+        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+      },
     ];
 
     shuffleArray(cardData);
