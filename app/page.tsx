afe3d90949e80bd340f4eeea4bc48dce96dd50ae
Mojibake <109202065+Mojibake-1@users.noreply.github.com>
2024-10-31 18:24:27 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -190,8 +190,9 @@ export default function Home() {
           rel="noopener noreferrer"
           style={{ color: "#d19b75", textDecoration: "none" }}
         >
-          体验Claude
-        </a> */}
+          体验Claude */}
+        <a style={{ color: "#808080" }}>Supported by DAO  </a>
+        </a>
       </div>
 
       <div className={styles.center}>
