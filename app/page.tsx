4183b87f7632afcef4e414440cfad83cbc952a84
Mojibake <109202065+Mojibake-1@users.noreply.github.com>
2024-11-27 12:49:20 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,130 +15,130 @@ export default function Home() {
     };
     const cardData = [
       {
-        title: "内外空间关系研究",
-        url: "https://neu1.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "星际自由移动",
-        url: "https://neu2.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :  80 messages /3 hours",
-      },
-      {
-        title: "偏蚀",
-        url: "https://neu3.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "万象",
-        url: "https://neu4.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "太阳流水线",
-        url: "https://neu5.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "电影宇宙",
-        url: "https://neu6.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "多样的未来主义",
-        url: "https://neu7.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "远程反射疗法",
-        url: "https://neu8.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "时空之间",
-        url: "https://neu9.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "66号公路",
-        url: "https://neu10.342707641.xyz/",
-        description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-  {
-        title: "走向天空的人",
-        url: "https://neu11.342707641.xyz/",
-        description: "Access to GPT-4o mini",
-      },
-      {
-        title: "轨道反射器",
-        url: "https://neu12.342707641.xyz/",
-        description: " Access to GPT-4o mini",
-      },
-            {
-        title: "洞穴之喻",
-        url: "https://neu13.342707641.xyz/",
-        description: " Access to GPT-4o mini",
-      },
-      {
-        title: "时钟总是错的",
-        url: "https://neu14.342707641.xyz/",
-        description: " Access to GPT-4o mini",
-      },
-      {
-        title: "二分之一个月亮",
-        url: "https://neu15.342707641.xyz/",
-        description: " Access to GPT-4o mini",
-      },
-      {
-        title: "关于无的无所有",
-        url: "https://neu16.342707641.xyz/",
-        description: " Access to GPT-4o mini",
-      },
-        {
-        title: "与……对话",
-        url: "https://neu17.342707641.xyz/",
-        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "午夜时刻",
-        url: "https://neu18.342707641.xyz/",
-        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-           {
-        title: "行走的花",
-        url: "https://neu19.342707641.xyz/",
-        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
-      {
-        title: "至上主义",
-        url: "https://neu20.342707641.xyz/",
-        description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
-      },
+  //       title: "内外空间关系研究",
+  //       url: "https://neu1.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "星际自由移动",
+  //       url: "https://neu2.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :  80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "偏蚀",
+  //       url: "https://neu3.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "万象",
+  //       url: "https://neu4.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "太阳流水线",
+  //       url: "https://neu5.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "电影宇宙",
+  //       url: "https://neu6.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "多样的未来主义",
+  //       url: "https://neu7.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "远程反射疗法",
+  //       url: "https://neu8.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "时空之间",
+  //       url: "https://neu9.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "66号公路",
+  //       url: "https://neu10.342707641.xyz/",
+  //       description: "Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  // {
+  //       title: "走向天空的人",
+  //       url: "https://neu11.342707641.xyz/",
+  //       description: "Access to GPT-4o mini",
+  //     },
+  //     {
+  //       title: "轨道反射器",
+  //       url: "https://neu12.342707641.xyz/",
+  //       description: " Access to GPT-4o mini",
+  //     },
+  //           {
+  //       title: "洞穴之喻",
+  //       url: "https://neu13.342707641.xyz/",
+  //       description: " Access to GPT-4o mini",
+  //     },
+  //     {
+  //       title: "时钟总是错的",
+  //       url: "https://neu14.342707641.xyz/",
+  //       description: " Access to GPT-4o mini",
+  //     },
+  //     {
+  //       title: "二分之一个月亮",
+  //       url: "https://neu15.342707641.xyz/",
+  //       description: " Access to GPT-4o mini",
+  //     },
+  //     {
+  //       title: "关于无的无所有",
+  //       url: "https://neu16.342707641.xyz/",
+  //       description: " Access to GPT-4o mini",
+  //     },
+  //       {
+  //       title: "与……对话",
+  //       url: "https://neu17.342707641.xyz/",
+  //       description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "午夜时刻",
+  //       url: "https://neu18.342707641.xyz/",
+  //       description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //          {
+  //       title: "行走的花",
+  //       url: "https://neu19.342707641.xyz/",
+  //       description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
+  //     {
+  //       title: "至上主义",
+  //       url: "https://neu20.342707641.xyz/",
+  //       description: " Access to GPT-4o\nrate limit :80 messages /3 hours",
+  //     },
       //   {
       //   title: "巴别塔",
       //   url: "https://neu21.342707641.xyz/",
       //   description: " Access to GPT-4o mini",
       // },
       //      {
-      //   title: "a",
-      //   url: "https://plus.aivvm.com/auth/login_share?token=fk-ohYtptgfDmYvgAWe87lmG4RjuLnGl8RGaIqyNSBVkVU",
-      //   description: " Access to GPT-4o\nrate limit :80 messages /3 hours（无对话历史记录",
-      // },     
-      // {
-      //   title: "b",
-      //   url: "https://plus.aivvm.com/auth/login_share?token=fk-YYIPTjPtiWqfPuNb9W4Eg_tXNQbPB6FaVO2MHdchEP8",
-      //   description: " Access to GPT-4o\nrate limit :80 messages /3 hours（无对话历史记录",
-      // },
-      //  {
-      //   title: "c",
-      //   url: "https://plus.aivvm.com/auth/login_share?token=fk-41fHogv7YygH6Z56aZo2Hjm4ENYmxlePP1EBj4env9E",
-      //   description: " Access to GPT-4o\nrate limit :80 messages /3 hours（无对话历史记录",
-      // },
-      //  {
-      //   title: "d",
-      //   url: "https://plus.aivvm.com/auth/login_share?token=fk-LzVGG_N0nT17t2qcaAp1JkUP20r-Z1rnVrp7iLWdD2w",
-      //   description: " Access to GPT-4o\nrate limit :80 messages /3 hours（无对话历史记录",
-      // },
+        title: "c1",
+        url: "https://fuclaudeshare.neugpt.workers.dev",
+        description: " Access to claude-sonnet",
+      },     
+      {
+        title: "c2",
+        url: "https://fuclaudeshare.neugpt.workers.dev",
+        description: " Access to claude-sonnet",
+      },
+       {
+        title: "c3",
+        url: "https://fuclaudeshare.neugpt.workers.dev",
+        description: " Access to claude-haiku",
+      },
+       {
+        title: "c4",
+        url: "https://fuclaudeshare.neugpt.workers.dev",
+        description: " Access to claude-haiku",
+      },
       //  {
       //   title: "e",
       //   url: "https://plus.aivvm.com/auth/login_share?token=fk-_gka-hmFHq1rIdLqUpmXqSpckvO3lGhkB1ljD1jct-8",
