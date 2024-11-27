54f6607b04050af1449df64cdbb654bd9dd62869
Mojibake <109202065+Mojibake-1@users.noreply.github.com>
2024-11-28 00:20:01 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -122,32 +122,42 @@ export default function Home() {
       //      {
         title: "仁",
         url: "https://31.learnwithgpt.space",
-        description: " Basic Account, access to claude-sonnet",
+        description: " Basic Plan, access to claude-sonnet",
       },     
       {
         title: "义",
         url: "https://32.learnwithgpt.space",
-        description: " Basic Account, access to claude-sonnet",
+        description: " Basic Plan, access to claude-sonnet",
       },
        {
         title: "礼",
         url: "https://33.learnwithgpt.space",
-        description: " Basic Account, access to claude-haiku",
+        description: " Basic Plan, access to claude-haiku",
       },
        {
         title: "智",
         url: "https://34.learnwithgpt.space",
-        description: " Basic Account, access to claude-haiku",
+        description: " Basic Plan, access to claude-haiku",
       },
           {
         title: "信",
         url: "https://35.learnwithgpt.space",
-        description: " Basic Account, access to claude-haiku",
+        description: " Basic Plan, access to claude-haiku",
       },
        {
         title: "忠",
         url: "https://36.learnwithgpt.space",
-        description: " Basic Account, access to claude-haiku",
+        description: " Basic Plan, access to claude-haiku",
+      },
+                {
+        title: "勇",
+        url: "https://37.learnwithgpt.space",
+        description: " Professional Plan, access to claude-sonnet",
+      },
+       {
+        title: "恕",
+        url: "https://38.learnwithgpt.space",
+        description: " Professional Plan, access to claude-sonnet",
       },
     ];
 
