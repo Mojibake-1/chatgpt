35f0ef06109df81b3a177b7c5743faeb0f8ad2fd
Mojibake <109202065+Mojibake-1@users.noreply.github.com>
2024-11-30 20:43:44 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -199,7 +199,7 @@ export default function Home() {
        <div className={styles.center}>
 {/* <p className={styles.tip}> */}
 <p>
- <a >因不可抗力，目前仅可用claude账号，chatgpt不确定什么时候能恢复 </a>
+ <a >chatgpt已有临时替代方案，即将更新 </a>
   </p>
   </div>
       <div className={styles.grid}>
